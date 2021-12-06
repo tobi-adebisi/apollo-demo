bdef6d1fcb83ea1c238d7e99ab59d7721bd6a7a0
Tobi Adebisi <tobi@apollographql.com>
2021-12-06 06:39:19 -0500

    Add tournaments for player and update rover check

--- games/resolvers.js
+++ games/resolvers.js
@@ -1,11 +1,29 @@
 
 const games = require('./games.json');
 
+function fetchGamesByPlayer(player){
+  var gamesByPlayer = [];
+
+  for (var i = 0; i < games.length; i++) {
+    if (games[i].whitePlayerName === player.name || 
+        games[i].blackPlayerName === player.name) {
+      gamesByPlayer.push(games[i]);
+    }
+  }
+
+  return gamesByPlayer;
+}
+
 const resolvers = {
     Query: {
       // returns an array of chess Games 
       games: () => games
     },
+    Player: {
+      games(player) {
+        return fetchGamesByPlayer(player);
+      }
+    }
   };
   
   module.exports = resolvers;
--- tournaments/resolvers.js
+++ tournaments/resolvers.js
@@ -1,10 +1,27 @@
 const tournaments = require('./tournaments.json');
 
+function fetchTournamnentsByPlayer(player){
+  var tournamnentsByPlayer = [];
+
+  for (var i = 0; i < tournaments.length; i++) {
+    if (tournaments[i].players.includes(player.name)) {
+      tournamnentsByPlayer.push(tournaments[i]);
+    }
+  }
+
+  return tournamnentsByPlayer;
+}
+
 const resolvers = {
     Query: {
       // returns an array of chess Tournaments 
       tournaments: () => tournaments
     },
+    Player: {
+      tournaments(player) {
+        return fetchTournamnentsByPlayer(player);
+      }
+    }
   };
   
   module.exports = resolvers;
